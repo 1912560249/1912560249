625c59c172e82d9b93317ebf5999de07249db125
chenyufeng <1912560249@qq.com>
2021-09-12 23:02:03 +0800

    第一次测试提交

--- src/main/java/com/test/springboot/SpringbootApplication.java
+++ src/main/java/com/test/springboot/SpringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication {
 
     public static void main(String[] args) {
+        //nice to metting you!
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
