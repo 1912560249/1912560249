1bd11cd4cdb8621ca15dd80f382c6f8008741e41
chenyufeng <1912560249@qq.com>
2021-09-21 20:33:30 +0800

    测试nice  20210921

--- src/main/java/com/test/springboot/SpringbootApplication.java
+++ src/main/java/com/test/springboot/SpringbootApplication.java
@@ -8,6 +8,7 @@ public class SpringbootApplication {
 
     public static void main(String[] args) {
         //nice to metting you!
+		//第二次提交，nice to metting you!
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
